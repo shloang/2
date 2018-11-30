36760c8a480487b2a6f9f1176f409a048349abd4
Dina Alys frey <dalys-fr@e3r2p13.21-school.ru>
2018-11-30 22:34:06 +0300

    daily final oof 1

--- libs/ft_memcpy.c
+++ libs/ft_memcpy.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalys-fr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/30 22:29:21 by dalys-fr          #+#    #+#             */
+/*   Updated: 2018/11/30 22:30:25 by dalys-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <strings.h>
 
 void	*ft_memcpy(void *destination, const void *source, size_t num)
 {
 	const char	*s;
-	char				*d;
-	size_t			i;
+	char		*d;
+	size_t		i;
 
 	s = source;
 	d = destination;
--- libs/ft_memmove.c
+++ libs/ft_memmove.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dalys-fr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/30 22:29:30 by dalys-fr          #+#    #+#             */
+/*   Updated: 2018/11/30 22:32:13 by dalys-fr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 
-void *ft_memmove (void *destination, const void *source, size_t num)
+void	*ft_memmove(void *destination, const void *source, size_t num)
 {
 	char				*d;
-	const char	*s;
+	const char			*s;
 	char				buf[num];
 	int					i;
 
